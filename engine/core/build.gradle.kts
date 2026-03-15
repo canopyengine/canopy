31d55f4cd24dfa963eaf9ba7dfcb06e61b149e2d
Guilherme Ferreira <guilherme.ferreira120703@gmail.com>
2026-03-15 17:17:03 +0000

    Squashed commit of the following:
    commit 5737813ec7399183075df28b1f9125e6e643f8a9
    Author: Guilherme Ferreira <97637719+GuilhermeF03@users.noreply.github.com>
    Date:   Sun Mar 15 17:16:05 2026 +0000
    
        Refactored Module's Dependencies (#47)
    
    commit a5e16b95cee5577f5b3430f73d70fef9dbfcaeed
    Author: Guilherme Ferreira <guilherme.ferreira120703@gmail.com>
    Date:   Sun Mar 15 16:52:11 2026 +0000
    
        Updated docs with

--- engine/core/build.gradle.kts
+++ engine/core/build.gradle.kts
@@ -11,28 +11,12 @@ val enableGraalNative: Boolean = providers
 
 dependencies {
     // Canopy
-    implementation(projects.engine.utils)
-    implementation(projects.engine.logging)
-
-    // Gdx
-    api(libs.gdx.core)
-
-    // Ktx
-    api(libs.ktx.app)
-    api(libs.ktx.assets.async)
-    api(libs.ktx.assets)
-    api(libs.ktx.async)
-    api(libs.ktx.log)
-    api(libs.ktx.math)
+    api(projects.engine.utils)
+    api(projects.engine.logging)
 
     // Logging
     api(libs.slf4j.api)
     runtimeOnly(libs.logback.classic)
-
-    // Graal helper only when enabled
-    if (enableGraalNative) {
-        implementation(libs.graal.helper.annotations)
-    }
 }
 
 // Canopy custom tasks
